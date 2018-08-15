bd493cd91f07301edad7e44d5651e526ee88a234
mooselumph <robert.raynor@gmail.com>
2018-08-14 18:53:28 -0600

    Added buefy dependency

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,21 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <NavBar/>
     <router-view/>
   </div>
 </template>
+
+<script>
+// @ is an alias to /src
+import NavBar from "@/components/NavBar.vue";
+
+export default {
+  name: "home",
+  components: {
+    NavBar
+  }
+};
+</script>
 
 <style lang="scss">
 #app {
--- src/router.js
+++ src/router.js
@@ -2,6 +2,11 @@ import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
 
+import Buefy from 'buefy'
+import 'buefy/lib/buefy.css'
+
+Vue.use(Buefy)
+
 Vue.use(Router);
 
 export default new Router({
